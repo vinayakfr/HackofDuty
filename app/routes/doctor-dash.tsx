6726b83beef39a6ddbb48bb377dd66a66f7cf865
vinayakfr <vinayak.sri@yahoo.com>
2024-10-28 17:10:13 +0530

    feat: add features on doctor-dash

--- app/routes/doctor-dash.tsx
+++ app/routes/doctor-dash.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Calendar from "~/components/calendar";
 
 function DoctorDashboard() {
   return (
@@ -14,21 +15,48 @@ function DoctorDashboard() {
               </p>
             </div>
             <div className="flex flex-col lg:flex-row justify-between gap-2 items-end w-full">
-              <div className="h-[27rem] bg-white rounded-2xl p-4 w-full lg:w-[60%]">
-                {/* box 4 */}
-              </div>
+              <Calendar />
               <div className="flex flex-row lg:flex-col justify-between gap-4 w-full lg:w-[40%]">
-                <div className="h-[13rem] bg-white rounded-2xl p-4 w-[50%] lg:w-auto">
-                  {/* box 5 */}
+                <div className="flex flex-col gap-5 place-content-center place-items-start h-[13rem] bg-white rounded-2xl w-[50%] lg:w-auto p-4">
+                  <h1 className="text-2xl font-medium">Quick Notes</h1>
+                  <textarea
+                    name="notes"
+                    id=""
+                    className="bg-gray-300 rounded-2xl p-2 text-lg h-[70%] w-full"
+                  ></textarea>
                 </div>
-                <div className="h-[13rem] bg-white rounded-2xl p-4 w-[50%] lg:w-auto ">
-                  {/* box 5 */}
+                <div className="flex flex-col gap-5 place-content-start place-items-start h-[13rem] bg-white rounded-2xl w-[50%] lg:w-auto p-4">
+                  <h1 className="text-2xl font-medium">Reminders </h1>
                 </div>
               </div>
             </div>
           </div>
-          <div className="h-[47rem] w-full lg:w-[30%] rounded-2xl p-4 bg-white">
-            {/* 1 box */}
+          <div className="flex flex-col gap-4 place-content-center place-items-start h-[47rem] w-full lg:w-[30%] rounded-2xl p-4 bg-white">
+            <h1 className="text-3xl font-medium">Next Appointment</h1>
+            <div className="flex justify-between items-center w-full bg-gray-300 rounded-2xl pr-2">
+              <img src="Patient.png " alt="" className="w-[50%] rounded-2xl" />
+              <div className="flex flex-col place-content-center place-items-start gap-3">
+                <h1>Sachin Singh</h1>
+                <h1>Male</h1>
+                <h1>9818367251</h1>
+              </div>
+            </div>
+            <div className="flex justify-between items-center w-full bg-gray-300 rounded-2xl pr-2">
+              <img src="Patient.png " alt="" className="w-[50%] rounded-2xl" />
+              <div className="flex flex-col place-content-center place-items-start gap-3">
+                <h1>Sachin Singh</h1>
+                <h1>Male</h1>
+                <h1>9818367251</h1>
+              </div>
+            </div>
+            <div className="flex justify-between items-center w-full bg-gray-300 rounded-2xl pr-2">
+              <img src="Patient2.jpg " alt="" className="w-[50%] rounded-2xl" />
+              <div className="flex flex-col place-content-center place-items-start gap-3">
+                <h1>Sachin Singh</h1>
+                <h1>Male</h1>
+                <h1>9818367251</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
