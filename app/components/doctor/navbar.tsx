815233c2e45b3b4e5583a26d6d58def667c93c83
Rishi Raj <rr1158@srmist.edu.in>
2024-10-28 22:29:02 +0530

    Doctor Nav bar and Footer added

--- app/components/doctor/navbar.tsx
+++ app/components/doctor/navbar.tsx
@@ -1,1 +1,21 @@
+import React from "react";
 
+function Navbar() {
+  return (
+    <div className="flex place-content-center place-items-center w-full">
+      <div className="flex justify-between items-center w-[97%] p-3 bg-gray-300 rounded-full mt-3">
+        <a href="/doctor-dash" className="text-4xl font-bold">
+          Sanjeevani
+        </a>
+        <a
+            href="/doctor-profile"
+            className="text-xl font-medium hover:underline"
+          >
+            Profile
+          </a>
+        </div>
+      </div>
+  );
+}
+
+export default Navbar;
